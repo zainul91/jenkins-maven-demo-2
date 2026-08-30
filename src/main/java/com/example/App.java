2decd81865e8adc337a9fbd679b170a980ce4a00
zainul91 <zainulabedin997@gmail.com>
2026-08-30 23:47:59 +0530

    Initial Maven project 2

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Zainul!");
+        System.out.println("Maven Project 2 is working!");
+        System.out.println("Jenkins Project 2 is working!");
+
+    }
+}
